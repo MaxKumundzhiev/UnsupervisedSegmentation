6f9c71b6aee0037f009f739a1234e71c21d9f28f
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-11-15 19:10:39 +0100

    Created entry_point for unzipping and removing data to work with.

--- entry_point.sh
+++ entry_point.sh
@@ -1,9 +1,15 @@
-# TBD
+#!/bin/bash
 
-# @TODO
-# 1. setup environment
+data_dir="./data/"
 
-# 2. unzip data
-# unzip data folder + delete zip
-# cmd = f'unzip {ROOT_DATASET_FOLDER}/annotated_data.zip -d {ROOT_DATASET_FOLDER}'
-# os.system(cmd)
+echo "Hello from Bash! Unzipping annotated_data.zip"
+
+unzip ./data/annotated_data.zip -d $data_dir
+echo "Hello from Bash! Unzipped annotated_data.zip to the: $data_dir"
+
+rm -r ./data/annotated_data.zip
+echo "Hello from Bash! Deleted annotated_data.zip"
+
+
+
+
