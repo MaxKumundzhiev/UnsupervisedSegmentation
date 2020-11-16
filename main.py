43456ad3b9580892a279560ed318f2478da16b7a
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-11-16 17:49:31 +0100

    Finilised preparation of backened for generating data to work with.

---
+++ main.py
@@ -0,0 +1,13 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+from utils.dataset_handler import DatasetProcessor
+
+if __name__ == '__main__':
+    DatasetProcessor().run()
--- utils/dataset_handler.py
+++ utils/dataset_handler.py
@@ -8,24 +8,29 @@
 # -------------------------------------------
 
 import os
-from tqdm import tqdm
 
+import nrrd
+import scipy.misc
 import nibabel as nib
+
+import pandas as pd
+from tqdm import tqdm
 
 
 class DatasetProcessor:
     def __init__(self):
+        self.fault_record = []
+        self.target_path = 'dataset'
         self.root_data_dir = 'data/annotated_data/slicer'
         self.folder_paths = os.listdir('data/annotated_data/slicer')
-        self.fault_record = []
 
     def validate_records(self):
-        valid_nii_records_paths, valid_nrdd_records_paths = [], []
+        valid_nii_records_paths, valid_nrrd_records_paths = [], []
         for folder in self.folder_paths:  # 1, 2, 3 ...
             source_folder_path = os.path.join(self.root_data_dir, folder)
             source_files_paths = os.listdir(source_folder_path)
 
-            if len (source_files_paths) != 2:
+            if len(source_files_paths) != 2:
                 self.fault_record.append(source_folder_path)
                 continue
 
@@ -33,7 +38,7 @@ class DatasetProcessor:
             for file in source_files_paths:
                 extensions.append (file.split ('.')[-1])
 
-            if set (extensions) != {'nii', 'nrrd'}:
+            if set(extensions) != {'nii', 'nrrd'}:
                 self.fault_record.append(source_folder_path)
                 continue
 
@@ -41,78 +46,90 @@ class DatasetProcessor:
                 if file.endswith ('nii'):
                     valid_nii_records_paths.append(os.path.join(source_folder_path, file))
                 else:
-                    valid_nrdd_records_paths.append(os.path.join(source_folder_path, file))
+                    valid_nrrd_records_paths.append(os.path.join(source_folder_path, file))
 
-        return valid_nii_records_paths, valid_nrdd_records_paths
+        return sorted(valid_nii_records_paths), sorted(valid_nrrd_records_paths)
+
+    def process_nii(self, original_path, target_folder, index):
+        rows = []
+        try:
+            nii_record = nib.load(original_path)
+            record_data = nii_record.get_fdata ()
+            image_dim = len(record_data.shape)
+
+            slices_number = record_data.shape[-1]  # e.g.: 54
+            height, width = record_data.shape[0], record_data.shape[1]  # e.g.: 128, 128
+            slice_resolution = f'{height}*{width}'
+
+            if image_dim == 3:
+                for slice_index in range(slices_number):
+                    slice_context = record_data[:, :, slice_index]
+                    scipy.misc.imsave(f'{target_folder}/{slice_index}_original.jpg', slice_context)
+
+                    buffer_row = {
+                        'record_index': index,
+                        'record_path': target_folder,
+                        'record_class': 'nii',
+                        'slice_index': slice_index,
+                        'slice_context': slice_context,
+                        'slice_resolution': slice_resolution,
+                        'slice_size': slice_context.shape,
+                    }
+                    rows.append(buffer_row)
+            return rows
+
+        except Exception as e:
+            print(f'[SILENCED] Error: {e} occurred for {original_path}')
+
+    def process_nrrd(self, original_path, target_folder, index):
+        rows = []
+        try:
+            nrrd_data, _ = nrrd.read(original_path)
+            image_channels = len(nrrd_data.shape)
+            slices_number = nrrd_data.shape[2]  # e.g.: 54
+            width, height = nrrd_data.shape[0], nrrd_data.shape[1]  # e.g.: 128, 128
+            slice_resolution = f'{width}*{height}'
+
+            if image_channels == 3:
+                for slice_index in range(slices_number):
+                    slice_context = nrrd_data[:, :, slice_index]
+                    scipy.misc.imsave(f'{target_folder}/{slice_index}_segmented.jpg', slice_context)
+
+                    buffer_row = {
+                        'record_index': index,
+                        'record_path': target_folder,
+                        'record_class': 'nrrd',
+                        'slice_index': slice_index,
+                        'slice_context': slice_context,
+                        'slice_resolution': slice_resolution,
+                        'slice_size': slice_context.shape,
+                    }
+
+                    rows.append(buffer_row)
+            return rows
+
+        except Exception as e:
+            print(f'[SILENCED] Error: {e} occurred for {original_path}')
+
+    def generate_dataset(self, nii_paths, nrrd_paths):
+        for index, (nii, nrrd) in enumerate(tqdm(zip(nii_paths, nrrd_paths))):
+
+            target_folder_path = f'{self.target_path}/{index}'
+            target_folder_original = f'{target_folder_path}/original'
+            target_folder_segmented = f'{target_folder_path}/segmented'
+
+            os.mkdir(target_folder_path)
+            os.mkdir(target_folder_original)
+            os.mkdir(target_folder_segmented)
+
+            nii_rows = self.process_nii(original_path=nii, target_folder=target_folder_original, index=index)
+            nrrd_rows = self.process_nrrd(original_path=nrrd, target_folder=target_folder_segmented, index=index)
+
+            result_df = pd.DataFrame
+
+    def run(self):
+        nii_records, nrrd_records = self.validate_records()
+        self.generate_dataset(nii_paths=nii_records, nrrd_paths=nrrd_records)
 
 
-    def preprocess_dataset(self):
-        nii_records, nrdd_records = validate_records()
 
-
-    #
-    # @staticmethod
-    # def get_files(full_folder_path):
-    #     files = os.listdir(full_folder_path)
-    #     return f'{full_folder_path}/{files[0]}', f'{full_folder_path}/{files[1]}'
-    #
-    # @staticmethod
-    # def capture_nii_image(nii_record_path):
-    #     rows = []
-    #
-    #     try:
-    #         record = nib.load(nii_record_path)
-    #         record_data = record.get_fdata()  # (128, 128, 54), where 54 denotes number of slices relied to the image
-    #         image_dim = len(record_data.shape)
-    #
-    #         slices_number = record_data.shape[-1]  # e.g.: 54
-    #         height, width = record_data.shape[0], record_data.shape[1]  # e.g.: 128, 128
-    #         slice_resolution = f'{height}*{width}'
-    #
-    #         if image_dim == 3:
-    #             for slice_index in range(slices_number):
-    #                 slice_context = record_data[:, :, slice_index]
-    #                 buffer_row = {
-    #                     'record_path': nii_record_path,
-    #                     'record_class': 'nii',
-    #                     'slices_index': slice_index,
-    #                     'slice_context': slice_context,
-    #                     'slice_resolution': slice_resolution,
-    #                     'slice_size': slice_context.shape,
-    #                 }
-    #                 rows.append(buffer_row)
-    #             return rows
-    #         else:
-    #             raise
-    #
-    #     except Exception as e:
-    #         print(f'[SILENCED] Error: {e} occurred for {nii_record_path}')
-    #
-    # def run(self):
-    #     failed_folders = []
-    #     rows = []
-    #
-    #     for _, folder in enumerate(tqdm(self.folder_paths)):
-    #         full_folder_path = os.path.join(self.root_data_dir, folder)
-    #         files = os.listdir(full_folder_path)
-    #
-    #         # check whether folder consist of expected .nii and .nrdd files
-    #         if len(files) < 2:
-    #             failed_folders.append(full_folder_path)
-    #             continue
-    #
-    #         # elif not [file.endswith('.nii') for file in files] and not [file.endswith('.nrdd') for file in files]:
-    #         #     failed_folders.append(full_folder_path)
-    #         #     continue
-    #
-    #         else:
-    #             if self.rename_flag:
-    #                 original_image_path, segmented_image_path = self.rename_files(full_folder_path, files)
-    #             else:
-    #                 original_image_path, segmented_image_path = self.get_files(full_folder_path)
-    #
-    #             buff_nii_rows = self.capture_nii_image(nii_record_path=original_image_path)
-    #             rows.append(buff_nii_rows)
-    #
-    #     return rows, failed_folders
-    #
